eeee41b99e5feca1aa4abcbb74fffbabc3ab102d
Felix D. Nguyen <58353771+felixngd@users.noreply.github.com>
2023-08-04 17:11:10 +0700

    Merge pull request #1 from felixngd/feature/file_name

---
+++ CodeGen.Runtime/AddressableKeyGroupData.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Wolffun.CodeGen.MagicString
+{
+    /// <summary>
+    /// Group all key by its Asset group or a label
+    /// </summary>
+    [CreateAssetMenu(fileName = "AssetKeyGroup", menuName = "AddressablesCodeGen/AssetKeyGroup", order = 1)]
+    public class AddressableKeyGroupData : UnityEngine.ScriptableObject
+    {
+        public string GroupOrLabelName;
+        public string[] Keys;
+        
+        public UniTask PreloadAsync<T>() where T : UnityEngine.Object
+        {
+            var tasks = new List<UniTask>();
+            for (var i = 0; i < Keys.Length; i++)
+            {
+                var task = AddressablesManager.LoadAssetAsync<T>(Keys[i]);
+                tasks.Add(task);
+            }
+            return UniTask.WhenAll(tasks);
+        }
+    }
+}
